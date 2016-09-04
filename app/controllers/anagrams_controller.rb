b6b7a50825149fa51ff42023f5296cf36ab8b801
Sean Luckett <whitecrane007@gmail.com>
2016-09-04 14:38:35 -0600

    Allow fetching anagrams for a word
    Also, allow for a limit on the returned anagrams count.

--- app/controllers/anagrams_controller.rb
+++ app/controllers/anagrams_controller.rb
@@ -1,15 +1,21 @@
 class AnagramsController < ApplicationController
+
   def fetch
     word = params[:word]
     sorted_word = word.chars.sort.join('')
 
     anagrams = Anagram.where(sorted_word: sorted_word).where.not(word: word)
 
+    if params[:limit]
+      slice = Range.new(0, params[:limit].to_i, :exclude)
+      anagrams = anagrams[slice]
+    end
 
     render json: build_json_response(anagrams), status: 200
   end
 
   private
+
   def build_json_response(anagrams)
     words = anagrams.each_with_object([]) do |anagram, words|
       words.push anagram.word
@@ -17,4 +23,5 @@ class AnagramsController < ApplicationController
 
     "{\"anagrams\": #{words}}"
   end
+  
 end
--- spec/requests/anagrams_spec.rb
+++ spec/requests/anagrams_spec.rb
@@ -1,17 +1,25 @@
 require 'rails_helper'
 
 describe 'Anagrams API' do
-  it 'adds words to corpus' do
+  before :each do
     post '/words.json', params: {words: %w(read dear dare)}
+  end
+
+  it 'adds words to corpus' do
     expect(response.status).to be 201
   end
 
   it 'returns anagrams for a word' do
-    post '/words.json', params: {words: %w(read dear dare)}
     expected_response = {anagrams: ['dear', 'dare']}.as_json
 
     get '/anagrams/read.json'
     expect(response.status).to be 200
     expect(json).to eq expected_response
+  end
+
+  it 'returns limited number of anagrams by request' do
+    get '/anagrams/read.json?limit=1'
+    expect(response.status).to be 200
+    expect(json['anagrams'].count).to eq 1
   end
 end
