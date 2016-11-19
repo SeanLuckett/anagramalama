fef5e61e2a4e5e27830c084a10e1bdcd9f5631ac
Sean Luckett <whitecrane007@gmail.com>
2016-11-19 15:28:01 -0700

    Make test suite use test fixture dictionary
    Anagram uses a giant dictonary file by default to validate words for the
    corpus. This changes Anagram during testing so it uses a much smaller
    file in spec/fixtures/.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -29,6 +29,14 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
+  config.before(:each) do
+    test_dictionary = Dictionary.new(
+      file_path: Rails.root.join('spec', 'fixtures', 'dictionary.txt')
+    )
+    allow_any_instance_of(Anagram).to receive(:dictionary)
+                                        .and_return(test_dictionary)
+  end
+
   # This option will default to `:apply_to_host_groups` in RSpec 4 (and will
   # have no way to turn it off -- the option exists only for backwards
   # compatibility in RSpec 3). It causes shared context metadata to be
